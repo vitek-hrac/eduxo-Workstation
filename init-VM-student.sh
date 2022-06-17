c1d54137819618a3dac104558faa0ab48b430c6c
jtihlarik <jtihlarik@eduxo.cz>
2022-06-17 09:34:03 +0200

    Update init-VM-student.sh

--- init-VM-student.sh
+++ init-VM-student.sh
@@ -105,7 +105,7 @@ fZy3vWjYrVSzAAAADnN5c2FkbWluQGVkdXhvAQIDBA==
 chmod 600 $HOME/.ssh/id_rsa
 
 # GIT clone
-git clone git@github.com:SSIPF/eduxo.git
+git clone git@github.com:eduxo/eduxo.git
 
 
 # Install upgrades and basic programs
