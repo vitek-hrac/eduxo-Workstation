c3740be234a0803915854512c7ce17f88be60554
jtihlarik <jtihlarik@eduxo.cz>
2022-10-17 18:39:54 +0200

    Update start.sh

--- rh124/start.sh
+++ rh124/start.sh
@@ -1,5 +1,5 @@
-# Dockerfile je ulozen ve stejnem adresari
+#!/bin/bash
 
-docker build -t CONTAINER_NAME .
-docker run -d -p 80:80 -p 22:22 --name NAZEV CONTAINER_NAME:latest
-docker container inspect NAZEV | grep -i IPAddress | tail -n 1 | tr -d ' "'
+docker build -t centos-rh124 .
+docker run -d -p 80:80 -p 22:22 --name rh124 centos-rh124:latest
+docker container inspect rh124 | grep -i IPAddress | tail -n 1 | tr -d ' "'
