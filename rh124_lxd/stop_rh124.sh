e7f9acaa1b4973f312695909ade05f3dd214fcb7
jtihlarik <jtihlarik@eduxo.cz>
2022-10-23 19:56:58 +0200

    Update stop_rh124.sh

--- rh124_lxd/stop_rh124.sh
+++ rh124_lxd/stop_rh124.sh
@@ -10,12 +10,12 @@
 echo -e '\n\e[0;92mZastavuji kontejnery...\e[0m'
 lxc stop servera
 lxc stop serverb
-wait 2
+sleep 2
 
 # Delete containers
 echo -e '\n\e[0;92mMazu kontejnery...\e[0m'
 lxc delete servera
 lxc delete serverb
-wait 2
+sleep 2
 
 echo -e '\n\e[0;92mHOTOVO\e[0m'
