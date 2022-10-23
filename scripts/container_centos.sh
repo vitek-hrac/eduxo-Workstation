a597236209b3ffd024d1411d54358982a15914b4
jtihlarik <jtihlarik@eduxo.cz>
2022-10-23 19:19:53 +0200

    Update container_centos.sh

--- scripts/container_centos.sh
+++ scripts/container_centos.sh
@@ -27,7 +27,7 @@ echo -e '\e[0;92mZadejte pozadovanou IP adresu kontejneru '$NAME' (ze site 10.20
 read IP
 
 # Create container
-echo -e '\n\e[0;92mVytvarim kontejner...\e[0m'
+echo -e '\n\e[0;92mVytvarim kontejner '$NAME'\e[0m'
 lxc launch images:centos/9-Stream $NAME
 
 lxc exec $NAME -- cd /etc/yum.repos.d/
@@ -35,19 +35,19 @@ lxc exec $NAME -- sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/centos.re
 lxc exec $NAME -- sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/centos.repo
 
 # Upgrade container
-echo -e '\e[0;92mAktualizuji kontejner...\e[0m'
+echo -e '\e[0;92mAktualizuji kontejner '$NAME'\e[0m'
 sleep 5
 lxc exec $NAME -- dnf update -y > /dev/null
 lxc exec $NAME -- dnf autoremove -y > /dev/null
 
 # Setting container
-echo -e '\e[0;92mNastavuji kontejner...\e[0m'
+echo -e '\e[0;92mNastavuji kontejner '$NAME'\e[0m'
 
 # Install SSH
 lxc exec $NAME -- dnf install openssh-server nano -y > /dev/null
 
 # Add user to container
-echo -e '\n\e[0;92mVytvoreni uzivatele sysadmin v kontejneru '$NAME':\e[0m'
+echo -e '\n\e[0;92mVytvorim uzivatele sysadmin v kontejneru '$NAME':\e[0m'
 lxc exec $NAME -- groupadd sysadmin
 lxc exec $NAME -- useradd -rm -d /home/sysadmin -s /bin/bash -g sysadmin -G wheel -u 1001 sysadmin
 lxc exec $NAME -- sh -c 'echo "sysadmin:Netlab!23" | chpasswd'
@@ -70,7 +70,7 @@ lxc start $NAME
 # echo -e '\e[0;92mPro nastaveni domain-name je nutne opravneni:\e[0m'
 sudo sh -c 'echo "'$IP'     '$NAME'.eduxo.lab	'$NAME'" >> /etc/hosts'
 
-echo -e '\n\e[0;92mKontejner je pripraven:\e[0m
+echo -e '\n\e[0;92mKontejner '$NAME' je pripraven:\e[0m
 Container-name: '$NAME'
 Domain-name: '$NAME'.eduxo.lab
 IP adresa: '$IP'\n'
