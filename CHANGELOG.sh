4cc7b0686dbd44c994c6de08bfdcbaf775eaa935
jtihlarik <jtihlarik@eduxo.cz>
2022-12-10 18:21:10 +0100

    Create CHANGELOG.sh

---
+++ CHANGELOG.sh
@@ -0,0 +1,26 @@
+# Changelog
+
+- Version: 1.0.0	30-Apr-2022 - Jaroslav
+	- new VM (all-in-one)
+
+- Version: 1.1.0	15-Jun-2022 - Jaroslav
+	- add docker for compatible with HAXAGON
+	- set web homepage on www.eduxo.cz
+	- add secret repo eduxo
+	- add VPN client ZeroTier
+	
+- Version: 1.1.1	26-Jun-2022 - Jaroslav
+	- troubleshoot VPN client ZeroTier
+
+- Version: 1.1.2	03-Sept-2022 - Jaroslav
+	- edit VirtualBox setting
+
+- Version: 1.1.3	05-Sept-2022 - Jaroslav
+	- rebuild on Xubuntu 20.04
+
+- Version: 1.2.0	01-Oct-2022 - Jaroslav
+	- new VM on Ubuntu Mate 22.04
+	- change homepage			
+
+- Version: 1.2.1	09-Oct-2022 - Jaroslav
+	- troubleshooting GNS3
