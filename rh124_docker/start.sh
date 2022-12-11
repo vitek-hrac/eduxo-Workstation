21e2d810c115b8873fad9d51474cbdc46c95dc18
jtihlarik <jtihlarik@eduxo.cz>
2022-12-11 13:28:29 +0100

    Update start.sh

--- rh124_docker/start.sh
+++ rh124_docker/start.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
+# Vytvoreni docker containeru CentOS:latest
 
-docker build -t centos-rh124 .
-docker run -h rh124.eduxo.lab -d -p 80:80 -p 22:22 --name rh124 centos-rh124:latest
-docker container inspect rh124 | grep -i IPAddress | tail -n 1 | tr -d ' "'
+# Test internet connection
+function check_internet() {
+  printf "Checking if you are online...\n"
+  wget -q --spider http://github.com
+  if [ $? -eq 0 ]; then
+    echo -e '\e[0;92mOnline. Continuing.\e[0m'
+  else
+    echo -e '\e[0;91mOffline. Go connect to the internet then run the script again.\e[0m'
+  fi
+}
+
+check_internet
+
+# Container 
+echo -e '\n\e[0;92mZadejte jmeno kontejneru:\e[0m'
+read NAME
+
+docker build -t $NAME .
+docker run -h $NAME.eduxo.lab -d -p 80:80 -p 22:22 --name $NAME $NAME:latest
+docker container inspect $NAME | grep -i IPAddress | tail -n 1 | tr -d ' "'
